468c035f3ee3131e9df13825801813d202ae6bb4
Wingszy <85103070+Wingszy@users.noreply.github.com>
2021-11-06 09:05:24 +0800

    Add files via upload

---
+++ 环形链表.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<functional>
+#include <unordered_set>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
+public:
+	bool hasCycle(ListNode *head) {
+		unordered_set<ListNode*> seen;
+		while (head != nullptr) {
+			if (seen.count(head)) {
+				return true;
+			}
+			seen.insert(head);
+			head = head->next;
+		}
+		return false;
+	}
+};
+
